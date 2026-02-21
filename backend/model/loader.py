6b849fef176dd28d9289cb110e382b4cbe7934fc
Tre3s-Byte <malmar1701@gmail.com>
2026-02-20 19:44:14 -0500

    modified to load multiple models

--- backend/model/loader.py
+++ backend/model/loader.py
@@ -5,25 +5,35 @@ from pathlib import Path
 _llm = None
 
 
-def get_model():
-    global _llm
+def load_model(model_path, n_gpu_layers, n_ctx):
+    return Llama(
+        model_path = model_path,
+        n_gpu_layers = n_gpu_layers,
+        n_ctx = n_ctx,
+        n_batch = 256,
+         n_threads=8,
+         verbose = False
+    )
 
-    if _llm is None:
-        base_dir = Path(__file__).resolve().parents[2]
-        model_path = (
-            base_dir
-            / "backend"
-            / "models"
-            / "qwen2.5-7b-instruct-q5_k_m-00001-of-00002.gguf"
-        )
+# def get_model():
+#     global _llm
 
-        _llm = Llama(
-            model_path=str(model_path),
-            n_gpu_layers=20,
-            n_ctx=4096,
-            n_batch=256,
-            n_threads=8,
-            verbose=False,
-        )
+#     if _llm is None:
+#         base_dir = Path(__file__).resolve().parents[2]
+#         model_path = (
+#             base_dir
+#             / "backend"
+#             / "models"
+#             / "qwen2.5-7b-instruct-q5_k_m-00001-of-00002.gguf"
+#         )
 
-    return _llm
+#         _llm = Llama(
+#             model_path=str(model_path),
+#             n_gpu_layers=20,
+#             n_ctx=4096,
+#             n_batch=256,
+#             n_threads=8,
+#             verbose=False,
+#         )
+
+#     return _llm
