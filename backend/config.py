3b7bd3943bf73b35a23cf3bea170ffab41a5941e
Tre3s-Byte <malmar1701@gmail.com>
2026-02-20 19:49:12 -0500

    added custom ctx, gpu_layers and model paths

--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,27 @@
 USE_GPU = True
 N_GPU_LAYERS = -1
+SAFE_CTX_SMALL = 2048
+SAFE_CTX_MEDIUM = 4096
+SAFE_CTX_LARGE = 4096
+
+MODEL_SMALL_PATH = "models/qwen-1.5b-q4.gguf"
+MODEL_MEDIUM_PATH = "models/phi3-mini.gguf"
+MODEL_LARGE_PATH = "models/qwen2.5-7b-instruct-q5_k_m.gguf"
+
+MODE_CONFIG = {
+    "small":{
+        "path": MODEL_SMALL_PATH,
+        "n_gpu_layers": "-1",
+        "n_ctx" : SAFE_CTX_SMALL
+    },
+    "medium":{
+        "path": MODEL_MEDIUM_PATH,
+        "n_gpu_layers": "30",
+        "n_ctx" : SAFE_CTX_MEDIUM
+    },
+    "large":{
+        "path": MODEL_LARGE_PATH,
+        "n_gpu_layers": "20",
+        "n_ctx" : SAFE_CTX_LARGE
+    }
+}
