1aa130bdcd0e6ba98bc918dacc656354bc7265af
Tre3s-Byte <malmar1701@gmail.com>
2026-02-23 21:12:12 -0500

    threads dynamically assigned per model

--- backend/model/loader.py
+++ backend/model/loader.py
@@ -1,5 +1,6 @@
 from llama_cpp import Llama
 from pathlib import Path
+import os
 
 #This function loads the model and make it available to generate the answers of every request
 _llm = None
@@ -10,30 +11,8 @@ def load_model(model_path, n_gpu_layers, n_ctx):
         model_path = model_path,
         n_gpu_layers = n_gpu_layers,
         n_ctx = n_ctx,
-        n_batch = 256,
-         n_threads=8,
-         verbose = False
+        n_batch = 1536,
+        n_threads=os.cpu_count() - 2,
+        verbose = False
     )
 
-# def get_model():
-#     global _llm
-
-#     if _llm is None:
-#         base_dir = Path(__file__).resolve().parents[2]
-#         model_path = (
-#             base_dir
-#             / "backend"
-#             / "models"
-#             / "qwen2.5-7b-instruct-q5_k_m-00001-of-00002.gguf"
-#         )
-
-#         _llm = Llama(
-#             model_path=str(model_path),
-#             n_gpu_layers=20,
-#             n_ctx=4096,
-#             n_batch=256,
-#             n_threads=8,
-#             verbose=False,
-#         )
-
-#     return _llm
